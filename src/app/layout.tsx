47283c42c0a5929d70fc86a7066deb5b7943dabf
dontfred <mail@frederikwgrimm.de>
2023-07-17 15:27:26 +0200

    Added Opengraph for seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,32 +6,32 @@ import { useTheme } from 'next-themes'
 const graphik = localFont({
   src: [
     {
-      path: "../lib/font/graphik-light-webfont.woff2",
+      path: "../../public/font/graphik-light-webfont.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../lib/font/graphik-lightitalic-webfont.woff2",
+      path: "../../public/font/graphik-lightitalic-webfont.woff2",
       weight: "300",
       style: "italic",
     },
     {
-      path: "../lib/font/graphik-medium-webfont.woff2",
+      path: "../../public/font/graphik-medium-webfont.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../lib/font/graphik-regular-webfont.woff2",
+      path: "../../public/font/graphik-regular-webfont.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../lib/font/graphik-regularitalic-webfont.woff2",
+      path: "../../public/font/graphik-regularitalic-webfont.woff2",
       weight: "400",
       style: "italic",
     },
     {
-      path: "../lib/font/graphik-semibold-webfont.woff2",
+      path: "../../public/font/graphik-semibold-webfont.woff2",
       weight: "600",
       style: "normal",
     },
