906aa0c5221719a06ee768bd2af89e9380e2c645
dontfred <mail@frederikwgrimm.de>
2023-07-27 14:55:02 +0200

    test-version

---
+++ src/app/test-webauthn/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React, { useCallback, useState } from 'react'
+import { useWebAuthn } from 'react-hook-webauthn'
+
+const rpOptions = {
+    rpName: "SilverWeb",
+    rpId: "localhost",
+}
+
+export default function TestWebAuthn() {
+    const [login, setLogin] = useState('')
+    const {getCredential, getAssertion} = useWebAuthn(rpOptions)
+  
+    const onChangeLogin = useCallback((e: any) => {
+      setLogin(e.target.value)
+    }, [])
+  
+    const onRegister = useCallback(async  () => {
+      const credential = await getCredential({
+        challenge: 'stringFromServer',
+        userDisplayName: login,
+        userId: login,
+        userName: login
+      })
+      console.log(credential)
+    }, [getCredential, login])
+  
+    const onAuth = useCallback(async () => {
+      const assertion =  await getAssertion({challenge: 'stringFromServer'})
+      console.log(assertion)
+    }, [getAssertion])
+  
+    return (
+      <div className="App">
+        <main>
+          <div className="section">
+            <input onInput={onChangeLogin} placeholder="login" type="text"/>
+          </div>
+           <div className="section">
+             <button onClick={onRegister} type="button">register</button>
+           </div>
+          <div className="del"/>
+          <div className="section">
+            <button onClick={onAuth} type="button">auth</button>
+          </div>
+        </main>
+      </div>
+    );
+  }
